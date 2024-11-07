bc6b7193ad3d79d76ef8ce803e4720124d8e5bca
nandipatirohith7 <nandipatirohith7@gmail.com>
2024-11-07 11:42:41 +0530

    main-1

--- Pages/userpages/UserHome.cshtml.cs
+++ Pages/userpages/UserHome.cshtml.cs
@@ -1,3 +1,6 @@
+using Azure.Identity;
+using E_commerce.Data.Context;
+using E_commerce.Data.Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,29 @@ namespace E_commerce.Pages.userpages
 {
     public class UserHomeModel : PageModel
     {
-        public void OnGet()
+        private readonly DBcontext dbcontext;
+
+        public List<product> Products { get; set; }
+
+        public UserHomeModel(DBcontext dbcontext)
         {
+            this.dbcontext = dbcontext;
+        }
+
+       
+        public void OnGet(string category = null, string subcategory = null)
+        {
+            ViewData["username"] = HttpContext.Session.GetString("UserName");
+            Products = dbcontext.Products
+                .Where(p => (category == null || p.Category == category) &&
+                            (subcategory == null || p.SubCategory == subcategory))
+                .ToList();
+        }
+
+        public IActionResult OnPostSignout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToPage("../Index");
         }
     }
 }
