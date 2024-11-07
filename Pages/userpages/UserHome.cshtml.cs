0ecbf512a55dc889daf173d5e40b50775aaa66b7
nandipatirohith7 <nandipatirohith7@gmail.com>
2024-11-07 16:32:01 +0530

    filters

--- Pages/userpages/UserHome.cshtml.cs
+++ Pages/userpages/UserHome.cshtml.cs
@@ -3,6 +3,7 @@ using E_commerce.Data.Context;
 using E_commerce.Data.Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_commerce.Pages.userpages
 {
@@ -12,19 +13,48 @@ namespace E_commerce.Pages.userpages
 
         public List<product> Products { get; set; }
 
+        public List<product> filterproducts { get; set; } = new List<product>();
+
+        public Dictionary<string, List<string>> categorySubcategoryGroups {  get; set; } = new Dictionary<string, List<string>>();
+
+
         public UserHomeModel(DBcontext dbcontext)
         {
             this.dbcontext = dbcontext;
         }
-
+        [BindProperty]
+        public string cate{  get; set; }
+        [BindProperty]
+        public string subcate { get; set; }
        
-        public void OnGet(string category = null, string subcategory = null)
+        public void OnGet(string category , string subcategory )
         {
             ViewData["username"] = HttpContext.Session.GetString("UserName");
-            Products = dbcontext.Products
-                .Where(p => (category == null || p.Category == category) &&
-                            (subcategory == null || p.SubCategory == subcategory))
-                .ToList();
+            Products = dbcontext.Products.ToList();
+            categorySubcategoryGroups = Products.GroupBy(p => p.Category).ToDictionary(g => g.Key, g => g.Select(p => p.SubCategory).Distinct().ToList());
+            if (!string.IsNullOrEmpty(category))
+            {
+                // Filter products by category and subcategory (example logic)
+                filterproducts = dbcontext.Products.Where(p => p.Category == category &&
+                                                         (string.IsNullOrEmpty(subcategory) || p.SubCategory == subcategory))
+                                             .ToList();
+                
+            }
+            else
+            {
+                filterproducts= Products;
+            }
+            
+        }
+
+        public IActionResult OnPostFilter(string category , string subcategory )
+        {
+            if (!string.IsNullOrEmpty(subcategory))
+            {
+                return RedirectToPage( new { category = category, subcategory = subcategory });
+            }
+            return RedirectToPage( new { category = category, subcategory="" });
+            
         }
 
         public IActionResult OnPostSignout()
